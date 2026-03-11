d7814979c80a0686a31ac0cd456be91ca96a0ad4
xiar5 <xiar5@>
2026-03-11 16:28:34 +0900

    Add NavigationArrow Component

---
+++ Assets/cabotya/NavigationArrow/NavigationArrow.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class NavigationArrow : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
